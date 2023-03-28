e3676e4e356eb2c21495de9c2916ee76a035770b
Dongho Kim <d.kim@student.reading.ac.uk>
2023-03-28 13:27:12 +0000

    Update mapper.py

--- mapper.py
+++ mapper.py
@@ -1,13 +1,4 @@
-'''
 import sys
-for line in sys.stdin:
-    item = line.strip().split(',')
-    passenger_id = item[0]
-    print '%s\t%s' % (passenger_id, 1)
-'''
-# 나중에 추가할 것
-# passenger id 정규표현식 확인해서 일치하는 경우만 key로 set
-# 예외처리
 
 for line in sys.stdin:
     line = line.strip()
