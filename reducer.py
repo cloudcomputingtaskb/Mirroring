882dba5e1f4cf289f322c0c2625486d5766d0d52
Dongho Kim <d.kim@student.reading.ac.uk>
2023-03-28 13:27:29 +0000

    Update reducer.py

--- reducer.py
+++ reducer.py
@@ -1,31 +1,3 @@
-'''
-import sys
-
-total_flight = 0
-max_flight = 0
-current_id = ""
-highest_id = ""
-
-for line in sys.stdin:
-    key, value = line.strip().split('\t')
-    value = int(value)
-
-    if current_id == key:
-        total_flight += value
-        if total_flight > max_flight:
-            max_flight = total_flight
-            highest_id = current_id
-    
-    else:
-        if current_id:
-            print '%s\t%d' % (current_id, total_flight)
-        total_flight = value
-        current_id = key
-
-if current_id == key:
-    print '%s\t%d' % (current_id, total_flight)
-'''
-    
 import sys
 word = ""
 current_word = ""
