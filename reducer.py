58bd012e80f7e54fb1422395851d09ce98a9e2c7
Dongho Kim <d.kim@student.reading.ac.uk>
2023-03-28 13:23:37 +0000

    Update reducer.py

--- reducer.py
+++ reducer.py
@@ -1,3 +1,4 @@
+'''
 import sys
 
 total_flight = 0
@@ -23,5 +24,26 @@ for line in sys.stdin:
 
 if current_id == key:
     print '%s\t%d' % (current_id, total_flight)
-
+'''
     
+import sys
+word = ""
+current_word = ""
+current_count = 0
+
+for line in sys.stdin:
+    line = line.strip()
+    word, count = line.split('\t', 1)
+    try:
+        count = int(count)
+    except ValueError:
+        continue
+    if current_word == word:
+        current_count += count
+    else:
+        if current_word:
+            print '%s\t%s' % (current_word, current_count)
+        current_count = count
+        current_word = word
+if current_word == word:
+    print '%s\t%s' % (current_word, current_count)
