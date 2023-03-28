e5c0f4984bb6f357153ff885bbe05195a4df7869
Dongho Kim <d.kim@student.reading.ac.uk>
2023-03-28 15:32:18 +0000

    Update mapper.py

--- mapper.py
+++ mapper.py
@@ -1,6 +1,15 @@
-import sys
-
-for line in sys.stdin:
-    item = line.strip().split(',')
-    passenger_id = item[0]
-    print('%s\t%s', passenger_id, 1)
+public static class UPMapper extends Mapper<LongWritable, Text, Text, IntWritable> {
+    private Text key = new Text();
+    private IntWritable value = new IntWritable();
+    public void map(LongWritable key, Text value, Context context) throws IOException, InterruptedException{
+        String line = value.toString();
+        String items[] = line.split("\t");
+        String country = items[2];
+        String population = items[4];
+        if(country.matches("^[\\w\\-]+$") && population.matches("^\\d{1,9}$")) {
+            key.set(country);
+            value.set(Integer.parseInt(population));
+            context.write(key, value);
+        }
+    }
+}
