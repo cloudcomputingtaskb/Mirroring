7653683965cdcb5d3cc13ea3e67d6a8fe21b3025
ae812096 <ae812096@student.reading.ac.uk>
2023-03-31 14:21:16 +0100

    fix syntax error

--- mapper.py
+++ mapper.py
@@ -4,4 +4,4 @@ import sys
 for line in sys.stdin:
     item = line.strip().split(',')
     passenger_id = item[0]
-    print('%s\t%s', passenger_id, 1)
+    print('%s\t%s' % (passenger_id, 1))
--- reducer.py
+++ reducer.py
@@ -1,21 +1,26 @@
+#!/usr/bin/env python
 import sys
-word = ""
-current_word = ""
-current_count = 0
+
+total_flight = 0
+max_flight = 0
+current_id = ""
+highest_id = ""
 
 for line in sys.stdin:
-    line = line.strip()
-    word, count = line.split()
-    try:
-        count = int(count)
-    except ValueError:
-        continue
-    if current_word == word:
-        current_count += count
+    key, value = line.strip().split('\t')
+    value = int(value)
+
+    if current_id == key:
+        total_flight += value
+        if total_flight > max_flight:
+            max_flight = total_flight
+            highest_id = current_id
+    
     else:
-        if current_word:
-            print(current_word, current_count)
-        current_count = count
-        current_word = word
-if current_word == word:
-    print(current_word, current_count)
+        if current_id:
+            print ('%s\t%d' % (current_id, total_flight))
+        total_flight = value
+        current_id = key
+
+if current_id == key:
+    print ('%s\t%d' % (current_id, total_flight))
