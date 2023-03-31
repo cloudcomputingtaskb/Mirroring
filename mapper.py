ab64d40a05bb6e019429ec0c06b9f07b3706b020
ae812096 <ae812096@student.reading.ac.uk>
2023-03-31 23:39:21 +0100

    error fix

--- mapper.py
+++ mapper.py
@@ -1,26 +1,7 @@
 #!/usr/bin/env python
 import sys
 
-total_flight = 0
-max_flight = 0
-current_id = ""
-highest_id = ""
-
 for line in sys.stdin:
-    key, value = line.strip().split('\t')
-    value = int(value)
-
-    if current_id == key:
-        total_flight += value
-        if total_flight > max_flight:
-            max_flight = total_flight
-            highest_id = current_id
-    
-    else:
-        if current_id:
-            print ('%s\t%d' % (current_id, total_flight))
-        total_flight = value
-        current_id = key
-
-if current_id == key:
-    print ('%s\t%d' % (current_id, total_flight))
+    item = line.strip().split(',')
+    passenger_id = item[0]
+    print('%s\t%s' % (passenger_id, 1))
