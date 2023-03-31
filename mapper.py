32327269844f3d9e9ae6b58adbcc163ff17152bd
ae812096 <ae812096@student.reading.ac.uk>
2023-03-31 12:02:01 +0100

    commit_test

--- mapper.py
+++ mapper.py
@@ -1,7 +1,16 @@
 import sys
-
+# 
 for line in sys.stdin:
+<<<<<<< HEAD
     line = line.strip()
     words = line.split()
     for word in words:
         print(word, 1)
+=======
+    #
+    words = line.strip().split(',')
+    #
+    for word in words:
+        #
+        print ('%s\t%s' % (word, 1))
+>>>>>>> e277d91 (commit_test)
