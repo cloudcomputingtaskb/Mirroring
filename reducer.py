f61c70567b846383f8356601d045c13032724fdd
ae812096 <ae812096@student.reading.ac.uk>
2023-03-31 12:15:40 +0100

    example

--- reducer.py
+++ reducer.py
@@ -1,11 +1,13 @@
+from operator import itemgetter
 import sys
-word = ""
-current_word = ""
+
+current_word = None
 current_count = 0
+word = None
 
 for line in sys.stdin:
     line = line.strip()
-    word, count = line.split()
+    word, count = line.split('\t', 1)
     try:
         count = int(count)
     except ValueError:
@@ -14,8 +16,9 @@ for line in sys.stdin:
         current_count += count
     else:
         if current_word:
-            print(current_word, current_count)
+            print ('%s\t%s' % (current_word, current_count))
         current_count = count
         current_word = word
+        
 if current_word == word:
-    print(current_word, current_count)
+    print ('%s\t%s' % (current_word, current_count))
