b81c93c204f4adfd4a95bc1692290e0290362e34
ae812096 <ae812096@student.reading.ac.uk>
2023-03-31 23:23:26 +0100

    error fixed

--- reducer.py
+++ reducer.py
@@ -2,19 +2,18 @@
 import sys
 
 total_flight = 0
-max_flight = 0
 current_id = ""
-highest_id = ""
 
 for line in sys.stdin:
-    key, value = line.strip().split('\t')
-    value = int(value)
+    key, value = line.strip().split()
+
+    try:
+        value = int(value)
+    except ValueError:
+        continue
 
     if current_id == key:
         total_flight += value
-        if total_flight > max_flight:
-            max_flight = total_flight
-            highest_id = current_id
     
     else:
         if current_id:
