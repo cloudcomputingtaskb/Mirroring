0012f0b057563005d60d6ec72571616c2d15b990
ae812096 <ae812096@student.reading.ac.uk>
2023-04-01 14:32:14 +0100

    Change the main file name

--- Mapred.py
+++ Mapred.py
@@ -9,8 +9,10 @@ def main():
     with open(filepath, encoding="utf8") as file:
         input_mapper = file.read().splitlines()
 
+    # Assigning available processors to a pool
     with mp.Pool(processes=mp.cpu_count()) as pool:
-
+        # Use pool.map() to have each processor iteratively execute input_mapper to mapper()
+        # Chunking to distribute input data to each processor as well
         output_mapper = pool.map(mapper, input_mapper, chunksize=int(len(input_mapper)/mp.cpu_count()))
         
         input_reducer = shuffler(output_mapper)
