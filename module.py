d7b35b21e0fae310aa266dad90712c030af0a6de
ae812096 <ae812096@student.reading.ac.uk>
2023-04-01 03:13:13 +0100

    proto type

--- module.py
+++ module.py
@@ -1,16 +1,34 @@
-# Frame of each module
-def mapper(x):
-    return (x, 1)
+# --- mapper function ---
+# Input  : line of chunk
+# Output : [key, value] - list
+#          [passenger_id, 1] in this case
+def mapper(line):
+    output_mapper = []
+    item = line.strip().split(',')
+    # When ID is located in the First column
+    id_ = item[0]
+    output_mapper.append([id_, 1])
+    return output_mapper
 
-def reducer(x, y):
-    return x+y
-
-def shuffler(output_mapper):
+# --- shuffler function ---
+# Input  : kv pairs in output of mappers
+# Output : {key: [values]} - dictionary
+def shuffler(kv):
     output_shuffler = {}
-    for key, value in output_mapper:
+    for line in kv:
+        key, value = line[0]
         if key not in output_shuffler:
             output_shuffler[key] = [value]
         else:
             output_shuffler[key].append(value)
     return output_shuffler
 
+# --- reducer function ---
+# Input  : 
+# Output : {key: value} - dictionary
+def reducer(kvs):
+    output_reducer = {}
+    key, values = kvs
+    output_reducer[key] = sum(values)
+
+    return output_reducer
--- threading_1.py
+++ threading_1.py
@@ -1,14 +1,15 @@
 import multiprocessing as mp
 from module import mapper, reducer, shuffler
-import sys
 
 def main():
+    with open("C:\AComp_Passenger_data_no_error.csv", encoding="utf8") as f:
+        input_mapper = f.read().splitlines()
+
     with mp.Pool(processes=mp.cpu_count()) as pool:
         # Using standard IO as input of mapper
-        input_mapper = sys.stdin
 
         output_mapper = pool.map(mapper, input_mapper, chunksize=int(len(input_mapper)/mp.cpu_count()))
-
+        
         input_reducer = shuffler(output_mapper)
         
         output_reducer = pool.map(reducer, input_reducer.items(), chunksize=int(len(input_reducer.keys())/mp.cpu_count()))
