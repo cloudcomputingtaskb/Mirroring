ced6d96deb82a197493c0dfa2a84d43456f553af
ae812096 <ae812096@student.reading.ac.uk>
2023-04-10 12:01:46 +0100

    adding a file saving

--- Mapred.py
+++ Mapred.py
@@ -1,5 +1,5 @@
 import multiprocessing as mp
-from module import mapper, reducer, shuffler
+from module import mapper, reducer, shuffler, find_highest_flights
 
 def main():
     # input file path
@@ -21,9 +21,22 @@ def main():
         
         # Unpack shuffled_data from dictionary to list for iteration
         input_reducer = [shuffled_data[i] for i in range(num_reducer)]
+
+        # Each output from reducers comes into one list
         output_reducer = pool.map(reducer, input_reducer)
 
-        print(output_reducer)
+        result = find_highest_flights(output_reducer)
+        print(result)
+    
+    # Save the results of reducers into a text file
+    with open("output_reducer.txt", "w") as file:
+        # Convert non-string items to string and list of strings to a single one
+        file.write("\n".join(str(x) for x in output_reducer))
+    
+    # Save the final result of map reduce
+    with open("highest_passenger.txt", "w") as file:
+        # Convert non-string items to string and list of strings to a single one
+        file.write("\n".join(str(x) for x in result))
         
 if __name__ == '__main__':
     main()
--- module.py
+++ module.py
@@ -35,27 +35,53 @@ def shuffler(kv_pairs, num_reducers):
 
 # --- reducer function ---
 # Input  : shuffled list of key-value pairs
-# Output : [(key: values)] - list of tuple
+# Output : [(key: value)] - list of tuple
 def reducer(shuffled_data):
+    # Set initial values for variables
     key = None
-
     current_key = ""
     current_value = 0
-
     reduced_list = []
 
     for kv_pair in shuffled_data:
         key, value = kv_pair
-    
+        # In this solution, value is fixed to 1 (integer), but in the general solution, value is likely to be a string, so the convert function is built in
+        try:
+            value = int(value)
+        except ValueError:
+            continue
+        # If the key (ID) from the previous loop is the same as the current key, add value.
         if current_key == key:
             current_value += value
+        # If the key (ID) from the previous loop is different from the current key, add the value calculated so far to the reduce result
         else:
             if current_key:
                 reduced_list.append((current_key, current_value))
             current_key = key
             current_value = value
-
+    # If the value in the last loop is valid, add it to the final result
     if current_key == key:
         reduced_list.append((current_key, current_value))
 
     return (reduced_list)
+
+# --- find the passenger(s) having had the highest number of flights
+# Input  : reduced list of key-value pairs
+# Output : highest key-value pair(s)
+def find_highest_flights(reduced_data):
+    #  Set initial values for variables
+    highest_passenger_group = []
+    highest_flights_count = 0
+
+    for kv_pairs in reduced_data:
+        for kv_pair in kv_pairs:
+            _, value = kv_pair
+            # Find the highest value in key-value pairs
+            if value > highest_flights_count:
+                highest_passenger_group = [kv_pair]
+                highest_flights_count = value
+            # If there are multiple passengers with the maximum value
+            elif value == highest_flights_count:
+                highest_passenger_group.append(kv_pair)
+    
+    return highest_passenger_group
